561330d8565978b4185d63f854d671890905e788
EspecializaTi <conta@especializati.com.br>
2017-03-08 09:15:24 -0300

    Atualização PHP 7.1
    Atualizar o setup.sh para a versão 7.1 do PHP

--- setup.sh
+++ setup.sh
@@ -25,9 +25,9 @@ phpmyadmin      phpmyadmin/mysql/app-pass       password $DEFAULTPASS
 EOF
 
 echo "--- Instalando pacotes basicos ---"
-sudo apt-get install vim curl python-software-properties git-core --assume-yes --force-yes
+sudo apt-get install software-properties-common vim curl python-software-properties git-core --assume-yes --force-yes
 
-echo "--- Adicionando repositorio do pacote PHP 5.6 ---"
+echo "--- Adicionando repositorio do pacote PHP ---"
 sudo add-apt-repository ppa:ondrej/php
 
 echo "--- Atualizando lista de pacotes ---"
@@ -37,7 +37,12 @@ echo "--- Instalando MySQL, Phpmyadmin e alguns outros modulos ---"
 sudo apt-get install mysql-server-5.5 mysql-client phpmyadmin --assume-yes --force-yes
 
 echo "--- Instalando PHP, Apache e alguns modulos ---"
-sudo apt-get install php5.6 apache2 libapache2-mod-php5.6 php5.6-curl php5.6-gd php5.6-mcrypt php5.6-mysql php5.6-mbstring php5.6-json php5.6-xml unzip php7.0-zip install php7.0-mbstring --assume-yes --force-yes
+sudo apt-get install php7.1 php7.1-common --assume-yes --force-yes
+sudo apt-get install php7.1-cli libapache2-mod-php7.1 php7.1-mysql php7.1-curl php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring zip unzip --assume-yes --force-yes
+
+echo "--- Habilitando o PHP 7.1 ---"
+sudo a2dismod php5
+sudo a2enmod php7.1
 
 echo "--- Habilitando mod-rewrite do Apache ---"
 sudo a2enmod rewrite
@@ -51,7 +56,7 @@ sudo mv composer.phar /usr/local/bin/composer
 
 echo "--- Instalando Banco NoSQL -> Redis <- ---" 
 sudo apt-get install redis-server --assume-yes
-sudo apt-get install php5.6-redis --assume-yes
+sudo apt-get install php7.1-redis --assume-yes
 
 # Instale apartir daqui o que você desejar 
 
